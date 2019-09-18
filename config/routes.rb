d617a524559103a758c7587cc5da596b77bac8dc
anna_rgmv <annaragimov@gmail.com>
2019-09-18 21:41:54 +0300

    feat(routes): new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'jewelry_pricing', to: 'items#new'
+  post 'jewelry_pricing', to: 'items#create'
 end
