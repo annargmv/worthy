4ec2608a3e63a8756244096a1ce1665ec06c07bc
anna_rgmv <annaragimov@gmail.com>
2019-09-18 21:31:33 +0300

    feat(item.rb):migration file

---
+++ db/migrate/20190918182734_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :item_type
+      t.decimal :item_carat_weight
+      t.string :item_cut
+      t.string :item_color
+      t.string :item_clarity
+      t.timestamps
+    end
+  end
+end
