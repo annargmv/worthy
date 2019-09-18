a70096bc1b4b15f94c178eba989ff0c0220e83ca
anna_rgmv <annaragimov@gmail.com>
2019-09-18 23:58:58 +0300

    fix(items_controller): small chnages in controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,18 @@
 class ItemsController < ApplicationController
   def new
-
+    @item = Item.new
   end
 
   def create
-
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to jewelry_pricing_path
   end
+end
+
+
+private
+
+def item_params
+  params.permit(:item_type, :item_carat_weight, :item_cut, :item_color, :item_clarity)
 end
