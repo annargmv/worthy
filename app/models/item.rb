6f2eacd43665ead8cd8edad92663e5f3fb84e5f4
anna_rgmv <annaragimov@gmail.com>
2019-09-20 02:59:30 +0300

    feat(item.rb):Adding model functionality

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,23 @@
 class Item < ApplicationRecord
+  validates :item_type, presence: true, length: { minimum: 3, maximum: 25 }
+  validates :item_carat_weight, presence: true
+  validates :item_cut, presence: true
+  validates :item_color, presence: true
+  validates :item_clarity, presence: true
+
+  def total_price
+    1*2
+  end
+
+  def cut_value
+
+  end
+
+  def color_value
+
+  end
+
+  def clarity_value
+
+  end
 end
