7c07541f77abdfbbe8e8cef0925006c0d150899e
anna_rgmv <annaragimov@gmail.com>
2019-09-21 00:14:34 +0300

    update:some general fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'jewelry_pricing', to: 'items#new'
   post 'jewelry_pricing', to: 'items#create'
 
-  resources :items, only: [:new, :create]
+  resources :items, only: %i[new create]
 end
