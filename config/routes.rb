d531f4df23ac3e4542185ed5020de5f627b49af9
anna_rgmv <annaragimov@gmail.com>
2019-09-23 02:36:03 +0300

    feat(raoutes): root_path

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root 'items#new'
   get 'jewelry_pricing', to: 'items#new'
   post 'jewelry_pricing', to: 'items#create'
 
